e04ac8fb506841671120928cd66eeec4683af349
haodemon <haodemon@gmail.com>
2019-05-26 19:48:34 +0300

    Initial Commit

---
+++ MemoApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MemoApp
+//
+//  Created by Alfred S. on 26/05/2019.
+//  Copyright © 2019 haodemon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
